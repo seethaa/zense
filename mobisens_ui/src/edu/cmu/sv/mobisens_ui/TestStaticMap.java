0ac4ecffd84af35cc8874867238f6224799fdee2
Himanshu <himanshupandey.in@gmail.com>
2013-11-09 18:12:08 -0800

    another test commit -- will break the build

---
+++ mobisens_ui/src/edu/cmu/sv/mobisens_ui/TestStaticMap.java
@@ -0,0 +1,130 @@
+package edu.cmu.sv.mobisens_ui;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import edu.cmu.sv.lifelogger.GoogleMapActivity.QueryGooglePlaces;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageView;
+
+public class TestStaticMap extends Activity {
+	ImageView img ;
+	Bitmap bm;
+	double latitude;
+	double longitude;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test_static_map);
+		img = (ImageView)findViewById(R.id.imageView1);
+		latitude = 38.418709;
+		longitude = -121.057419;
+		new LongOperation.execute("");
+		bm = getGoogleMapThumbnail(38.418709, -121.057419);
+		img.setImageBitmap(bm);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.test_static_map, menu);
+		return true;
+	}
+	
+	
+
+
+    private class LongOperation extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+        	String URL = "http://maps.google.com/maps/api/staticmap?center=" +latitude + "," + longitude + "&zoom=15&size=200x200&sensor=false";
+            Bitmap bmp = null;
+            HttpClient httpclient = new DefaultHttpClient();   
+            HttpGet request = new HttpGet(URL); 
+
+            InputStream in = null;
+            try {
+                in = httpclient.execute(request).getEntity().getContent();
+                bmp = BitmapFactory.decodeStream(in);
+                in.close();
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return bmp;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            /*TextView txt = (TextView) findViewById(R.id.output);
+            txt.setText("Executed"); // txt.setText(result);
+            */// might want to change "executed" for the returned string passed
+            // into onPostExecute() but that is upto you
+            
+            //return bmp;
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static Bitmap getGoogleMapThumbnail(double lati, double longi){
+        String URL = "http://maps.google.com/maps/api/staticmap?center=" +lati + "," + longi + "&zoom=15&size=200x200&sensor=false";
+        Bitmap bmp = null;
+        HttpClient httpclient = new DefaultHttpClient();   
+        HttpGet request = new HttpGet(URL); 
+
+        InputStream in = null;
+        try {
+            in = httpclient.execute(request).getEntity().getContent();
+            bmp = BitmapFactory.decodeStream(in);
+            in.close();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return bmp;
+    }
+
+
+
+}
